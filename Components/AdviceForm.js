6992fc4a2428bab35fc8a840d88b151ac9b5ba6d
Caroline Forrester <carolineforrester22@gmail.com>
2022-12-04 20:12:59 -0500

    Added the components folder and created the AdviceForm.js file

---
+++ Components/AdviceForm.js
@@ -0,0 +1,19 @@
+import React, {useState, useRef} from 'react'
+
+
+function AdviceForm(){
+
+    const [randomAdvice, setReandomAdvice] = useState('')
+    const [query, setQuery] = useState('')
+    const [message, setMessage] = useState('')
+
+
+
+
+
+
+
+
+}
+
+export default AdviceForm;
