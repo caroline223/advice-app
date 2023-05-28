ce350ea3cdf1970ddb94c227928376216d6e9b0f
Caroline Forrester <carolineforrester22@gmail.com>
2023-05-27 20:16:06 -0400

    Adjusted styling features for the search bar

--- src/Components/AdviceForm.js
+++ src/Components/AdviceForm.js
@@ -71,15 +71,21 @@ function AdviceForm(){
             <p>{randomAdvice}</p>
 
             <div>
-                <h3>Type a Keyword Below to Search For Advice</h3>
-                <input
-                    ref={queryInput}
-                    required
-                    type="text"
-                />&nbsp;
-                    <button onClick={searchSubmit} type="submit">Results</button>
+                <h3>Are you experiencing stress? Doubt? Unsure of your next move?</h3>
+                <h4>Type a Keyword Below to Search For Advice</h4>
+                    <div style={{textAlign: 'center'}}>
+                        <input
+                            ref={queryInput}
+                            required
+                            type="text"
+                        />
+                    </div>
+                &nbsp;
+                <div style={{textAlign: 'center'}}>
+                    <button onClick={searchSubmit} type="submit">Results</button>&nbsp;&nbsp;&nbsp;&nbsp;
                     <button onClick={clearSearchInfo} type="submit">Clear</button>
-                <p style={{textAlign: 'center'}}>{query}</p>
+                </div>
+                <p>{query}</p>
             </div>
         </div>
     )
